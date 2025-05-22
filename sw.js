b34028511b57e9ed423527fd6e793a3e7fd895c8
dkmapks <dkmap039@gmail.com>
2025-05-22 09:26:43 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('narcosys-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/style.css',
+        '/js/main.js',
+        '/manifest.json',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
